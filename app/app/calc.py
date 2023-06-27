054565c231311351118cd2d459a8deec3afed72c
Muhammed Basheer <basheerkomassery@gmail.com>
2023-06-27 12:11:31 +0530

    Edited calc.py and tests.py to correct formatting errors based on pep8 standard

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,12 +2,14 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return result"""
-    
+
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract x and y and return the result"""
-    
+  
     return x - y
